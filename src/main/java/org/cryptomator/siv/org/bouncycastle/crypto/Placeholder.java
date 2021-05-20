b8a7361c21ce3d9a1d648177023271b5b108a425
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-05-20 08:41:34 +0200

    export shaded bouncycastle packages that are exposed to public API

---
+++ src/main/java/org/cryptomator/siv/org/bouncycastle/crypto/Placeholder.java
@@ -0,0 +1,8 @@
+package org.cryptomator.siv.org.bouncycastle.crypto;
+
+/**
+ * module-info will be evaluated before maven-shade-plugin, so we need this placeholder
+ * to avoid complaints about this package being empty.
+ */
+class Placeholder {
+}
